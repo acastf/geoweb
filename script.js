2117686992716bd135a9bb85a7d5fbe16386c4a2
acastf <58954659+acastf@users.noreply.github.com>
2020-02-16 01:34:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -26,20 +26,69 @@ function drawLine(marray) {
 function testFunction1(){
     layerGroup.clearLayers();
     mymap.closePopup();
-    marker = L.marker([40.743, -73.822]).addTo(layerGroup);
-    marker.bindPopup("Spiderman").openPopup();
+    marker = L.marker([35.6894989, 139.6917114]).addTo(layerGroup);
+    marker.bindPopup("Tokio: 39,4 millones").openPopup();
 }
 
 function testFunction2(){
     layerGroup.clearLayers();
     mymap.closePopup();
-    marker = L.marker([39.760979, -84.192200]).addTo(layerGroup);
-    marker.bindPopup("Hulk").openPopup();
+    marker = L.marker([23.1166706, 113.25]).addTo(layerGroup);
+    marker.bindPopup("Cantón: 32,6 millones").openPopup();
 }
 
 function testFunction3(){
     layerGroup.clearLayers();
     mymap.closePopup();
-    marker = L.marker([54.464180, -110.182259]).addTo(layerGroup);
-    marker.bindPopup("Wolverine").openPopup();
+    marker = L.marker([31.2222195, 121.4580612]).addTo(layerGroup);
+    marker.bindPopup("Shanghái: 29,6 millones").openPopup();
+}
+
+function testFunction4(){
+    layerGroup.clearLayers();
+    mymap.closePopup();
+    marker = L.marker([-6.2146201, 106.8451309]).addTo(layerGroup);
+    marker.bindPopup("Yakarta: 27 millones").openPopup();
+}
+
+function testFunction5(){
+    layerGroup.clearLayers();
+    mymap.closePopup();
+    marker = L.marker([28.6519508, 77.2314911]).addTo(layerGroup);
+    marker.bindPopup("Delhi: 25,3 millones").openPopup();
+}
+
+function testFunction6(){
+    layerGroup.clearLayers();
+    mymap.closePopup();
+    marker = L.marker([37.5660019, 126.9784012]).addTo(layerGroup);
+    marker.bindPopup("Seúl: 24,2 millones").openPopup();
+}
+
+function testFunction7(){
+    layerGroup.clearLayers();
+    mymap.closePopup();
+    marker = L.marker([30.3753204, 69.3451157]).addTo(layerGroup);
+    marker.bindPopup("Karachi: 23,2 millones").openPopup();
+}
+
+function testFunction8(){
+    layerGroup.clearLayers();
+    mymap.closePopup();
+    marker = L.marker([19.0728302, 72.8826065]).addTo(layerGroup);
+    marker.bindPopup("Bombay: 22,6 millones").openPopup();
+}
+
+function testFunction9(){
+    layerGroup.clearLayers();
+    mymap.closePopup();
+    marker = L.marker([14.6042004, 120.9822006]).addTo(layerGroup);
+    marker.bindPopup("Manila: 22,5 millones").openPopup();
+}
+
+function testFunction10(){
+    layerGroup.clearLayers();
+    mymap.closePopup();
+    marker = L.marker([19.4284706, -99.1276627]).addTo(layerGroup);
+    marker.bindPopup("Ciudad de México: 22,2 millones").openPopup();
 }
